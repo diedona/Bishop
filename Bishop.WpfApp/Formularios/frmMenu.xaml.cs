9c5e0013b46580ffbea4283841cbc81bb893fc6f
Diego Doná <diedona@gmail.com>
2020-09-04 22:32:07 -0300

    adicionando busca

--- Bishop.WpfApp/Formularios/frmMenu.xaml.cs
+++ Bishop.WpfApp/Formularios/frmMenu.xaml.cs
@@ -5,6 +5,7 @@ using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
 using System.Windows.Documents;
+using System.Windows.Forms; // necessário dar unload no projeto e adicionar <UseWindowsForms>true</UseWindowsForms>
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
@@ -20,6 +21,17 @@ namespace Bishop.WpfApp.Formularios
         public frmMenu()
         {
             InitializeComponent();
+        }
+
+        private void btnBuscarRepositorio_Click(object sender, RoutedEventArgs e)
+        {
+            using (var dialog = new FolderBrowserDialog())
+            {
+                if (dialog.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+                {
+                    txtCaminhoRepositorio.Text = dialog.SelectedPath;
+                }
+            }
         }
     }
 }
